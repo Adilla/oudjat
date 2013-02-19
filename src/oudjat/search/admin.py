7547c09871ba4880cbb3c7ed5e5d4df7e7c52871
Adilla <adilla.susungi@gmail.com>
2013-02-19 12:18:04 +0100

    commit

---
+++ src/oudjat/search/admin.py
@@ -0,0 +1,13 @@
+from search.models import Word
+from search.models import Domain
+from search.models import Option
+from search.models import Research
+from django.contrib import admin
+
+class OptionAdmin(admin.ModelAdmin):
+     list_display = ('name', 'description')
+
+admin.site.register(Word)
+admin.site.register(Domain)
+admin.site.register(Option, OptionAdmin)
+admin.site.register(Research)
