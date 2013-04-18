d1d0791e41d86e871f4d2f8930572f4e8d5fe5eb
Adilla <adilla.susungi@gmail.com>
2013-04-18 10:52:13 +0200

    fabfile modified

--- fabfile.py
+++ fabfile.py
@@ -6,16 +6,13 @@ import fabtools
 
 env.hosts = ['localhost']
 
-
 SOURCE = 'source /usr/local/bin/virtualenvwrapper.sh'
 BIN = '/bin/bash -l -c '
 VENV_NAME = 'test'
 WORKON = 'workon ' + VENV_NAME 
 
-
-
 def virtualenv(venv_command, venv_name):
-   with cd('.'):
+   with prefix('cd /home/adilla/Bureau/oudjat/'):
       local(''+ BIN + '"' + SOURCE + ' && ' + venv_command + ' ' + venv_name + '"')
    
 
@@ -23,8 +20,6 @@ def setup():
    """ 
    Creating new virtualenv
    """
-
- 
    virtualenv('mkvirtualenv --no-site-package', VENV_NAME)
 
 
@@ -32,61 +27,32 @@ def install_requirements():
    """ 
    Installing required packages
    """
-   
-
-   with cd('.'), prefix(WORKON):
-      # require.deb.packages([
-      #       'python-dev',
-      #       'libmysqlclient-dev',
-      #       ])
-
-      
-      sudo('apt-get install python-dev')
-      sudo('apt-get install libmysqlclient-dev')
-      # require.python.package([
-      #       'coverage',
-      #       'rt',
-      #       'django==1.4',
-      #       'MySQL-python',
-      #       'google-api-python-client',
-      #       ])
-                           
-   # with cd('.'), prefix(WORKON):
+   with prefix('cd /home/adilla/Bureau/oudjat/'):
+      with prefix(WORKON):
+         run('pip install -r requirements.txt')
+         run('python setup.py install')
  
-
-   #    #requirement.txt ??
-      run('pip install coverage')
-      run('pip install rt')
-      run('pip install django==1.4')
-      run('pip install MySQL-python')
-      run('pip install --upgrade google-api-python-client')
-
 
 def run_tests():
    """ 
    Running tests with Coverage and unittest
    """
-   with cd('.'), prefix(WORKON):
-      run('python manage.py test report search')
-
+   with prefix('cd /home/adilla/Bureau/oudjat/src/oudjat/'):
+      with prefix(WORKON):
+         run('coverage run --source=\'.\' --branch manage.py test report search')
 
 
 def clean():
    """ 
    Removing created virtualenv
    """
- 
    virtualenv('rmvirtualenv', VENV_NAME) 
-
-
-
-
-
 
 
 def do_all():
    setup()
    install_requirements()
-   #run_tests()
+   run_tests()
    clean()
+
 
